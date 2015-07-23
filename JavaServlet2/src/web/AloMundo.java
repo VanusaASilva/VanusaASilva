47c980d95ecb108a0a97ac86598473d2723de505
Júlio Machado <julioapm@gmail.com>
2015-07-23 14:55:53 -0300

    Versão com exemplo JavaServlet2

---
+++ JavaServlet2/src/web/AloMundo.java
@@ -0,0 +1,48 @@
+package web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class AloMundo
+ */
+public class AloMundo extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html;charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		String nome = "";
+		try {
+			nome = request.getParameter("nome");
+			out.println("<html>");
+			out.println("<head>");
+			out.println("<title>Servlet AloMundo</title>");
+			out.println("</head>");
+			out.println("<body>");
+			out.println("<h1>Alô " + nome + "!</h1>");
+			out.println("</body>");
+			out.println("</html>");
+		} finally {
+			if(out != null) {
+				out.close();
+			}
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
